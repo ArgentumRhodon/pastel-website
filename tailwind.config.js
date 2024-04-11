2ce22b0cfaf2ef9dcc4f4e05a5190d6c99718afc
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-04-11 16:43:15 -0400

    page bg and title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        mainPage: "url('/src/imgs/mainPage.png')",
+      },
+    },
   },
   plugins: [],
 };
